16207019844ea3155dbba9f648a00befe8b30ef2
anshuldaksh2016 <anshuldaksh2016@gmail.com>
2020-11-29 15:11:22 +0530

    added myFunctionList component with mouse over and out

--- functionator/script.js
+++ functionator/script.js
@@ -1,5 +1,9 @@
 let myBlock;
 
+let myfunctionList;
+
+let funList = []; // global array we can add commands 
+
 document.addEventListener("DOMContentLoaded", function () {
     myBlock = document.createElement("div");
     myBlock.innerHTML = "Hello world ";
@@ -14,35 +18,63 @@ document.addEventListener("DOMContentLoaded", function () {
     myBlock.style.top = "100px";
     myBlock.style.left = "150px";
 
-
-
     document.body.appendChild(myBlock);
+    myfunctionList = document.createElement("div");
+    document.body.appendChild(myfunctionList);
 })
 
 document.addEventListener("keydown", function (e) {
     e.preventDefault();
     let keyC = e.keyCode;
     if (keyC === 37)
-        goLeft();
+        addFun("left");
     else if (keyC === 39)
-        goRight();
+        addFun("right");
     else if (keyC === 38)
-        goTop();
+        addFun("up")
+
     else if (keyC === 40)
-        goDown();
+        addFun("down")
+    else if (keyC === 67) {
+        myBlock.style.backgroundColor = randomColor();
+    }
 
 })
 
+function addFun(val) {
+    funList.push(val);
+    let span = document.createElement("span");
+    span.textContent = "+" + val;
+    span.style.padding = "10px";
+    span.style.border = "1px solid #ddd";
+
+    span.addEventListener("mouseover", function () {
+        this.style.backgroundColor = "purple";
+        this.style.color = "white";
+
+    })
+
+    span.addEventListener("mouseout", function () {
+        this.style.backgroundColor = "white";
+        this.style.color = "black"
+
+    })
+    myfunctionList.appendChild(span);
+    console.log(funList);
+}
+
+
+
+
 function randomColor() {
-    return Math.random().toString(16).substr(-6);
+    return "#" + Math.random().toString(16).substr(-6);
 }
 
 function goLeft() {
     let temp = myBlock.offsetLeft;
     temp = temp - 50;
     myBlock.style.left = temp + "px";
-    const color = "#" + randomColor();
-    myBlock.style.backgroundColor = color;
+    myBlock.style.backgroundColor = randomColor();
 
 }
 
@@ -50,23 +82,20 @@ function goRight() {
     let temp = myBlock.offsetLeft;
     temp = temp + 50;
     myBlock.style.left = temp + "px";
-    const color = "#" + randomColor();
-    myBlock.style.backgroundColor = color;
+    myBlock.style.backgroundColor = randomColor();
 }
 
 function goTop() {
     let temp = myBlock.offsetTop;
     temp = temp - 50;
     myBlock.style.top = temp + "px";
-    const color = "#" + randomColor();
-    myBlock.style.backgroundColor = color;
+    myBlock.style.backgroundColor = randomColor();
 }
 function goDown() {
     let temp = myBlock.offsetTop;
     temp = temp + 50;
     myBlock.style.top = temp + "px";
-    const color = "#" + randomColor();
-    myBlock.style.backgroundColor = color;
+    myBlock.style.backgroundColor = randomColor();
 }
 
 
