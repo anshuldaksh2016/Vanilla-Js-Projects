4604ccf4e243a87d85cb5d275535844b2e100510
anshuldaksh2016 <anshuldaksh2016@gmail.com>
2020-11-29 12:18:02 +0530

    styling using js

--- functionator/script.js
+++ functionator/script.js
@@ -3,5 +3,18 @@ let myBlock;
 document.addEventListener("DOMContentLoaded", function () {
     myBlock = document.createElement("div");
     myBlock.innerHTML = "Hello world ";
+
+    myBlock.style.width = "100px"
+    myBlock.style.height = "100px"
+    myBlock.style.backgroundColor = "purple";
+    myBlock.style.color = "white";
+    myBlock.style.lineHeight = "100px";
+    myBlock.style.textAlign = "center";
+    myBlock.style.position = "absolute";
+    myBlock.style.top = "100px";
+    myBlock.style.left = "150px";
+
+
+
     document.body.appendChild(myBlock);
 })
